057db1e2787e438ab145dcb921d5251cee784e4f
BadSnail11 <104677095+BadSnail11@users.noreply.github.com>
2023-12-08 13:39:38 +0300

    word geberation0

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
 import random
 
-def baraban():
-    return 1
+def word_gen():
+    f = open("words.txt")
+    words = []
+    for line in f.readlines():
+        words.append(line.split("#"))
+    f.close()
+    r = random.randint() % 50
+    return  words[r]
