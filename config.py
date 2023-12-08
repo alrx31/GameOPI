dbdde203508febb033af524fa0fa382b8aeffbbc
Alex <usov.lesha2017@gmail.com>
2023-12-08 19:37:19 +0300

    add function result

--- config.py
+++ config.py
@@ -8,3 +8,21 @@ def word_gen():
     f.close()
     r = random.randint() % 50
     return  words[r]
+
+
+def result(pl1,pl2,pl3,word){
+    max_result = max(
+        pl1.count,
+        pl2.count,
+        pl3.count,
+    )
+
+    if(max_result == pl1.count):
+        print("The Winner is :" + pl1.name + " with result: " + max_result) 
+    
+    if(max_result == pl2.count):
+        print("The Winner is :" + pl2.name + " with result: " + max_result)
+    
+    if(max_result == pl3.count):
+        print("The Winner is :" + pl3.name + " with result: " + max_result)
+}
